5f7bbb8b8a39c0252f3238396d43588c1ea6d12d
Felipe <felipe.amaya.7x@gmail.com>
2026-06-10 10:07:52 -0500

    Se elimina la carpeta del backend del monorepo

--- ServiceMuscleRice/dist/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getProductos, getPedidos, getDetallePedido, getPagos } from '../controllers/productController.js';
-const router = express.Router();
-router.get('/productos', getProductos);
-router.get('/pedidos', getPedidos);
-router.get('/detalle-pedido', getDetallePedido);
-router.get('/pagos', getPagos);
-export default router;
